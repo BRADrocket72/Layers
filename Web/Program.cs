885032dfe7926168fdf551d8ec88b5252f83f6ab
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2023-01-23 15:37:39 -0500

    added main url to show how to input

--- Web/Program.cs
+++ Web/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 FactorialServicer factorialService = new FactorialServicer();
+app.MapGet("/", () => "Insert a number in the url like 'localhost:5000/1");
+
 app.MapGet("/{i}", (int i) => printResult(i));
 
 string printResult(int i)
